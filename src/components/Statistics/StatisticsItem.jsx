130305b96e412d8bf968d2344383d15964547320
DmytroSvarkovskiy <svarkovskii722@gmail.com>
2022-11-25 15:37:57 +0200

    -

--- src/components/Statistics/StatisticsItem.jsx
+++ src/components/Statistics/StatisticsItem.jsx
@@ -1,8 +1,8 @@
-export const StatisticsItem = ({ data: { label, percentage } }) => {
-  return (
-    <>
-      <span>{label}</span>
-      <span>{percentage}</span>
-    </>
-  );
-};
+// export const StatisticsItem = ({ data: { label, percentage } }) => {
+//   return (
+//     <>
+//       <span>{label}</span>
+//       <span>{percentage}</span>
+//     </>
+//   );
+// };
--- src/components/Statistics/StatisticsSection.jsx
+++ src/components/Statistics/StatisticsSection.jsx
@@ -1,21 +1,27 @@
 import PropTypes from 'prop-types';
-import { StatisticsItem } from './StatisticsItem';
 import data from '../../source/data.json';
 export const StatisticsSection = ({ title }) => {
   return (
     <section>
       {title && <h2>{title}</h2>}
-
       <ul>
-        {data.map(element => (
-          <li key={element.id}>
-            <StatisticsItem data={element} />
+        {data.map(({ id, label, percentage }) => (
+          <li key={id}>
+            <span>{label}</span>
+            <span>{percentage}</span>
           </li>
         ))}
       </ul>
     </section>
   );
 };
-StatisticsItem.propTypes = {
-  label: PropTypes.string,
+StatisticsSection.propTypes = {
+  title: PropTypes.string,
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+      percentage: PropTypes.number.isRequired,
+    })
+  ),
 };
